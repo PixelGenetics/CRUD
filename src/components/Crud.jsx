ecfbddc634fa7098957aec24f79b7a6dd59b05eb
Kevin Antonio Borge Gutierrez <108099077+PixelGenetics@users.noreply.github.com>
2024-02-09 18:50:43 -0600

    Changing logic.

--- src/components/Crud.jsx
+++ src/components/Crud.jsx
@@ -1,30 +1,70 @@
-import { useEffect,useState} from "react"
-import {useForm} from "react-hook-form"
-
-import axios from 'axios'
+import React, { useEffect } from "react";
+import { useForm} from "react-hook-form";
+import axios from 'axios';
+import useCrud from "./useCrud";
 
 const Crud = () => {
+    const { register, handleSubmit } = useForm();
+    const url = 'http://users-crud.academlo.tech/';
+    const [users, getUsers, createUsers, deleteUsers, updateUsers] = useCrud(url)
 
-    axios.get('http://users-crud.academlo.tech/users/').then(resp => {
-        console.log('resp',resp)
-    })
 
-    const {register, handleSubmit} = useForm()
-    console.log('hola')
-    return(
+    useEffect (() => {
+        getUsers('/users/');
+    },[])
+
+    console.log(users);
+
+    const onSubmit = (data) => {
+        createUsers('/users',data);
+        reset({
+            email:'',
+            password: '',
+            first_name: '',
+            last_name: '',
+            birthday: '',
+        })
+    }
+
+    useEffect(() => {
+        getUsers('/users/');
+    },[])
+
+
+    return (
         <div>
-            <p>Hola mundo</p>
-        
-            <form onSubmit={handleSubmit((data) => {
-                console.log("formulario enviado", data)})}>
-                <label htmlFor="nombre">Nombre</label>
-                <input type="text" {...register("nombre")}/> 
+            <div>
+            <p>Formulario</p>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email">email</label>
+                <input type="mail" {...register("email")} />
+
+                <label htmlFor="password">password</label>
+                <input type="text" {...register("password")} />
+
+                <label htmlFor="first_name">first_name</label>
+                <input type="text" {...register("first_name")} />
+
+                <label htmlFor="last_name">last_name</label>
+                <input type="text" {...register("last_name")} />
+
+                <label htmlFor="birthday">birthday</label>
+                <input type="date" {...register("birthday")} />
+
+                <label htmlFor="image_url">image_url</label>
+                <input type="text" {...register("image_url")} />
 
                 <button type="submit">Enviar</button>
             </form>
-        
+            </div>
+
+            <div>
+
+            </div>
+
         </div>
     )
 }
 
-export default Crud
+export default Crud;
